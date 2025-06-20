e5ae52d3e3581e5a6b5b12dc38640328afb0254f
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-06-20 17:23:52 +0900

    +

---
+++ Assignments/11-dars/11-dars.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>11 Dars</title>
+
+    </head>
+    <body>
+        <?php
+            $color = "red";
+            echo "Hello World" .$color . "<br>";
+            echo "Hello World" .$Color . " <br>";
+            echo "Hello World" .$COLor . "<br>";
+        ?>
+    </body>
+</html> 
